b1e876712a4a36351309ac7033e0b9bb87f183d3
doyeon <kdy@neurophet.com>
2025-09-11 11:19:01 +0900

    save_to_excel.py 잘못된 파라미터 수정

--- AQ32/t1/save_to_excel.py
+++ AQ32/t1/save_to_excel.py
@@ -1,3 +1,5 @@
 
-def save_to_excel(results_df, output_excel_file_path: str, rois):
-    results_df.to_excel(output_excel_file_path)
+def save_to_excel(out_df, output_excel_file_path: str, rois):
+    out_df.to_excel(output_excel_file_path)
+
+
